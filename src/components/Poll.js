540705cb6882100ca6d4e3c1e64f586e3fd448fe
Rich Razon <rich@carrot.co>
2019-04-05 16:21:27 -0700

    Implements vote and voted views

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,37 +1,82 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getPercentage } from '../utils/helpers'
 
- class Poll extends Component {
+const getVoteKeys = () => ['aVotes', 'bVotes', 'cVotes', 'dVotes']
+
+class Poll extends Component {
+  handleAnswer = (answer) => {
+    const { poll, authedUser } = this.props
+    this.answered = true
+
+    console.log('Add Answer:', answer)
+  }
   render() {
+    if (this.props.poll === null) {
+      return <p>This poll does not exist</p>
+    }
+
+    const { poll, vote, authorAvatar } = this.props
+
+    const totalVotes = getVoteKeys()
+      .reduce((total, key) => total + poll[key].length, 0)
+
     return (
       <div className='poll-container'>
-        {JSON.stringify(this.props)}
+        <h1 className='question'>
+          {poll.question}
+        </h1>
+        <div className='poll-author'>
+          By <img src={authorAvatar} alt="Author's avatar" />
+        </div>
+        <ul>
+          {['aText', 'bText', 'cText', 'dText'].map((key) => {
+            const count = poll[key[0] + 'Votes'].length
+
+            return (
+              <li
+                onClick={() => {
+                  if (vote === null && !this.answered) {
+                    this.handleAnswer(key[0])
+                  }
+                }}
+                className={`option ${vote === key[0] ? 'chosen' : ''}`}>
+                  {vote === null
+                    ? poll[key]
+                    : <div className='result'>
+                        <span>{poll[key]}</span>
+                        <span>{getPercentage(count, totalVotes)}% ({count})</span>
+                      </div>}
+              </li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
 }
 
- function mapStateToProps ({ authedUser, polls, users}, { match }) {
+function mapStateToProps ({ authedUser, polls, users}, { match }) {
   const { id } = match.params
   const poll = polls[id]
 
-   if (!poll) {
+  if (!poll) {
     return {
       poll: null
     }
   }
 
-   const vote = ['aVotes', 'bVotes', 'cVotes', 'dVotes'].reduce((vote, key) => {
+  const vote = getVoteKeys().reduce((vote, key) => {
     if (vote !== null) {
       return vote[0]
     }
 
-     return poll[key].includes(authedUser)
+    return poll[key].includes(authedUser)
       ? key
       : vote
   }, null)
 
-   return {
+  return {
     poll,
     vote,
     authedUser,
@@ -39,4 +84,4 @@ import { connect } from 'react-redux'
   }
 }
 
- export default connect(mapStateToProps)(Poll)
+export default connect(mapStateToProps)(Poll)
